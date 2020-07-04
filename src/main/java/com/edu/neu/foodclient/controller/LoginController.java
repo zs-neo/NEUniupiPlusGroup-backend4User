ab8535b6bff59db7055d40ae94c4cf45b630e78d
chanchenwee <chanchenwee@gmail.com>
2020-07-04 14:29:18 +0800

    completeUpdatePwd&Icon

--- src/main/java/com/edu/neu/foodclient/controller/LoginController.java
+++ src/main/java/com/edu/neu/foodclient/controller/LoginController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author zhousheng
@@ -28,9 +29,11 @@ public class LoginController {
 	private LoginService loginService;
 	
 	@RequestMapping("/login")
-	public Client login(String username, String password) {
+	public Client login(String username, String password, HttpSession session) {
 		System.out.println(username+password);
-		return loginService.login(username,password);
+		Client client = loginService.login(username, password);
+		session.setAttribute("user", client);
+		return client;
 	}
 
 	@RequestMapping("/register")
